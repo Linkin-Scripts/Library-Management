c6f9ded182cdab937f1a117a5b30533f57f796e5
daniel-morg-silva <dms121970@gmail.com>
2026-03-23 18:18:57 +0100

    Created the project

---
+++ Library Management App/ViewModels/HomeViewModel.cs
@@ -0,0 +1,6 @@
+namespace Library_Management_App.ViewModels;
+
+public partial class HomeViewModel : ViewModelBase
+{
+    public string Text { get; } = "Main Page";
+}
---
+++ Library Management App/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Library_Management_App.Views;
+using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Library_Management_App.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    private LoginView LoginView {get;} = new() { DataContext= new LoginViewModel() };
+    private HomeView HomeView {get;} = new() { DataContext= new HomeViewModel() };
+
+    [ObservableProperty]
+    private UserControl _currentView;
+
+    public MainWindowViewModel()
+    {
+        CurrentView = LoginView;
+    }
+
+    public void NextView()
+    {
+        if (CurrentView == LoginView)
+        {
+            CurrentView = HomeView;
+        }
+        else
+        {
+            CurrentView = LoginView;
+        }
+ 
+    }
+
+}
